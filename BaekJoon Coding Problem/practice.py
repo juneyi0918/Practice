22472353e4bd5122e3f3eecdca0842afc0c6e61b
June Yi <yijune@hawaii.edu>
2022-05-14 11:11:26 -1000

    commit

--- BaekJoon Coding Problem/practice.py
+++ BaekJoon Coding Problem/practice.py
@@ -1,11 +1,1 @@
 T = int(input())
-for _ in range(T):
-    q = input()
-    N = int(input())
-    li = [int(input()) for i in range(N)]
-    print("YES" if sum(li)%N == 0 else "NO")
-    
-    
-    
-    
-    
