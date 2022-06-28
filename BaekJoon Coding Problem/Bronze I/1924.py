789216664ccd36bbcb859bf6d0dd4cd8a986be13
June Yi <yijune@hawaii.edu>
2022-06-27 22:10:05 -1000

    2022-06-27

---
+++ BaekJoon Coding Problem/Bronze I/1924.py
@@ -0,0 +1,9 @@
+day=0
+lst=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+wklst=["SUN", "MON","TUE", "WED", "THU", "FRI", "SAT"]
+
+x,y=map(int,input().split())
+for i in range(x-1):
+    day+=lst[i]
+day=(day+y)%7
+print(wklst[day])
